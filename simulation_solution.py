34cb23ebbfbcdbc8dcf01e439104d3811b9bbc86
felipegerhardt <flc.gerhardt@gmail.com>
2021-02-13 22:19:54 -0300

    Changes in solution file

--- simulation_solution.py
+++ simulation_solution.py
@@ -1,6 +1,5 @@
-# Monitoring time
-import time
-time_start = time.time()
+# Testing System class
+from pydy.system import System
 
 # Loading previous solutions
 from equations_of_motion_solution import *
@@ -11,7 +10,7 @@ from sympy import init_printing
 from numpy import deg2rad, rad2deg, array, zeros, linspace
 
 # Import ODE numerical integration routine from SciPy
-from scipy.integrate import odeint
+from scipy.integrate import odeint  # Not using this function
 
 # Import PyDy's ODE function generation to generate the symbolic equations into numerical function
 from pydy.codegen.ode_function_generators import generate_ode_function
@@ -19,6 +18,7 @@ from pydy.codegen.ode_function_generators import generate_ode_function
 # Import a few functions from Matplotlib.PyPlot
 from matplotlib.pyplot import plot, legend, xlabel, ylabel, rcParams
 
+# Creating a list with the symbolic constants
 constants = [lower_arm_length,               # Symbolic form
              lower_arm_com_length,
              lower_arm_mass,
@@ -41,106 +41,66 @@ constants = [lower_arm_length,               # Symbolic form
              finger2_inertia,           
              g]
 
-################################ Solution that works
-specifieds = [lambda1, lambda2, lambda3, lambda4, lambda5]
+torques = [ground_joint_torque, lower_arm_joint_torque, upper_arm_joint_torque, finger1_joint_torque, finger2_joint_torque]
+
+coordinates = [lambda1, lambda2, lambda3, lambda4, lambda5]
 
 speeds = [omega1, omega2, omega3, omega4, omega5]
 
+# Creating time domain as an array
 time_span = 10
-
 time_points = 60*10
-
 import numpy as np
-
 t = np.linspace(0,time_span,time_points, dtype=np.float64)
 
-# Creating the torques list
-torques = [ground_joint_torque, lower_arm_joint_torque, upper_arm_joint_torque, finger1_joint_torque, finger2_joint_torque]
-
 # Passing the RHS to the ODE function generator
-right_hand_side = generate_ode_function(forcing_vector, torques, speeds, constants, mass_matrix=mass_matrix, specifieds=specifieds)
+right_hand_side = generate_ode_function(forcing_vector, coordinates, speeds, constants, mass_matrix=mass_matrix, specifieds=torques)
 
 # Initial conditions 
-x0 = np.zeros(10)
+x0 = {lambda1: np.deg2rad(90),
+      lambda2: np.deg2rad(0),
+      lambda3: np.deg2rad(0),
+      lambda4: np.deg2rad(0),
+      lambda5: np.deg2rad(0),
+      omega1: 0.0,
+      omega2: 0.0,
+      omega3: 0.0,
+      omega4: 0.0,
+      omega5: 0.0}
 
-numerical_constants = array([1,        # lower_arm_length [m]
-                             0.44309,  # lower_arm_com_length [m]
-                             347.2,    # lower_arm_mass [kg]
-                             49.945,   # lower_arm_inertia [kg*m^2]
-                             1.62475,  # upper_arm_length [m]
-                             0.56774,  # upper_arm_com_length
-                             61.10,    # upper_arm_mass [kg]
-                             18.80,    # upper_leg_inertia [kg*m^2]
-                             0.6428,   # hand_length [m]
-                             0.1424,   # hand_com_length [m]
-                             7.33,     # hand_mass [kg]
-                             0.31035,  # hand_inertia [kg*m^2]
-                             0.4218,   # finger1_length [m]
-                             0.2077,   # finger1_com_length [m]
-                             0.5,      # finger1_mass [kg]
-                             0.007322, # finger1_inertia [kg*m^2]
-                             0.4218,   # finger2_length [m]
-                             0.2077,   # finger2_com_length [m]
-                             0.5,      # finger2_mass [kg]
-                             0.007322, # finger2_inertia [kg*m^2
-                             9.81], dtype=np.float64)   # acceleration due to gravity [m/s^2]
+# Numerical constants
+numerical_constants = {lower_arm_length: 1.0,         # lower_arm_length [m]   
+                       lower_arm_com_length: 0.44309, # lower_arm_com_length [m]
+                       lower_arm_mass: 347.2,         # lower_arm_mass [kg]
+                       lower_arm_inertia: 49.945,     # lower_arm_inertia [kg*m^2]
+                       upper_arm_length: 1.62475,     # upper_arm_length [m]
+                       upper_arm_com_length: 0.56774, # upper_arm_com_length [m]
+                       upper_arm_mass: 61.10,         # upper_arm_mass [kg]
+                       upper_arm_inertia: 18.80,      # upper_leg_inertia [kg*m^2]
+                       hand_length: 0.6428,           # hand_length [m]
+                       hand_com_length: 0.1424,       # hand_com_length [m]
+                       hand_mass: 7.33,               # hand_mass [kg]
+                       hand_inertia: 0.31035,         # hand_inertia [kg*m^2]
+                       #finger1_length: 0.4218,       # finger1_length [m]          Doesn't matter/Not on the mass or forcing matrices
+                       finger1_com_length: 0.2077,    # finger1_com_length [m]
+                       finger1_mass: 0.5,             # finger1_mass [kg]
+                       finger1_inertia: 0.007322,     # finger1_inertia [kg*m^2]
+                       #finger2_length: 0.4218,       # finger2_length [m]           Doesn't matter/Not on the mass or forcing matrices
+                       finger2_com_length: 0.2077,    # finger2_com_length [m]
+                       finger2_mass: 0.5,             # finger2_mass [kg]
+                       finger2_inertia: 0.007322,     # finger2_inertia [kg*m^2]
+                       g: 9.81}                       # acceleration due to gravity [m/s^2]
 
-# Create a numpy array of the numerical specified torques
-def coordinates(t):
-    return -t/1000
+numerical_specifieds = {ground_joint_torque: lambda lambda1, t: 10*np.sin(10*t),
+                        lower_arm_joint_torque: lambda lambda2, t: 10*np.sin(10*t),
+                        upper_arm_joint_torque: lambda lambda3, t: 10*np.sin(10*t),
+                        finger1_joint_torque: lambda lambda4, t: 10*np.sin(10*t),
+                        finger2_joint_torque: lambda lambda5, t: 10*np.sin(10*t)}
 
-numerical_specified =  np.array([1*coordinates(t), 
-                                 2*coordinates(t), 
-                                 3*coordinates(t), 
-                                 4*coordinates(t), 
-                                 5*coordinates(t)], dtype=np.float64)
+sys = System(kane,
+             constants=numerical_constants,
+             specifieds=numerical_specifieds,
+             initial_conditions=x0,
+             times=t)
 
-numerical_specified = np.reshape(numerical_specified,
-                                (np.shape(numerical_specified)[1], np.shape(numerical_specified)[0]))
-
-time_end = time.time()
-
-print(f'Lapsed time: {time_end-time_start:.2f}s')
-
-#print(right_hand_side(x0, 10.0, numerical_specified[0], numerical_constants))
-
-help(right_hand_side)
-
-# Integrate the ODE 
-y = odeint(right_hand_side, x0, t, args=(numerical_specified, numerical_constants))
-
-################################
-
-
-
-"""
-def l_func(t, tspan, lambda_final, lambda_initial): # Generalized coordinate
-    
-    t = time vector (numpy array)
-    tspan = total time span of the simulation (int)
-    lambda_final = final degree (int)
-    lambda_initial = initial degree (int)
-    
-    return np.deg2rad(t*(lambda_final-lambda_initial)/time_span+lambda_initial)
-
-def o_func(tspan, lambda_final, lambda_initial): # Velocities
-    
-    tspan = total time span of the simulation (int)
-    lambda_final = final degree (int)
-    lambda_initial = initial degree (int)
-    
-    return np.deg2rad(lambda_final-lambda_initial)/time_span
-
-
-specified = [lambda1,                       # Symbolic form
-             lambda2,
-             lambda3,
-             lambda4,
-             lambda5]"""
-
-
-"""numerical_specified = array([[l_func(t, time_span, 45, 80)],
-                             [l_func(t, time_span, 45, 80)],
-                             [l_func(t, time_span, 45, 80)],
-                             [l_func(t, time_span, 45, 80)],
-                             [l_func(t, time_span, 45, 80)]],)"""
+y = sys.integrate()
